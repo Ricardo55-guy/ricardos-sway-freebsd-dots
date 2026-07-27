0bcf413ffad43b08a106a9f386263831690df9a8
Ricardo55-guy <ricardoromeroesq@gmail.com>
2026-07-27 04:40:54 -0400

    Alright

---
+++ setup_desktop.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sudo pkg update
+sudo pkg install -y sway wofi firefox nautilus git ufetch
+
+sudo sysrc seatd_enable="YES"
+sudo service seatd start
+
+sudo pw groupmod video -m $(whoami)
+sudo pw groupmod input -m $(whoami)
+
+mkdir -p ~/.config/sway
+cp -r config ~/.config/sway/
+cp -r scripts ~/.config/sway/
+chmod +x ~/.config/sway/scripts/*
+mkdir -p ~/Pictures/Wallpapers/
+cp flowers.jpg /Pictures/Wallpapers/
+
+echo "==========================================================="
+echo "                  Installation complete!                   "
+echo "  You can launch your desktop environment by typing: sway  "
+echo "==========================================================="
