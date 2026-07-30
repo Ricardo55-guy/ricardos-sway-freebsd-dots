340b357e8fc785ab66a5de9cac37710fa0aa5ecf
Ricardo <ricardoromeroesq@gmail.com>
2026-07-30 03:22:43 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -26,4 +26,4 @@ echo "  You can launch your desktop environment by typing: sway  "
 echo "==========================================================="
 echo "You can also use the extra wallpapers by copying them into the newly created ~/Pictures/Wallpapers."
 echo "Also, please load the drivers if you haven't by:
-echo "eg. for Intel, kldload i915kms for AMD, kldload amdgpu." 
+echo "eg, for Intel, kldload i915kms for AMD, kldload amdgpu." 
